a3cee74ac8ffbd64e13b395f4a94f312233a2ca8
vieerr <olivier_paspuel@hotmail.com>
2025-02-27 08:31:10 -0500

    feat: user analytics

---
+++ src/utils/icons.jsx
@@ -0,0 +1,12 @@
+import { MdElderly, MdElderlyWoman, MdMan, MdWoman } from "react-icons/md";
+import { TbManFilled, TbWomanFilled } from "react-icons/tb";
+
+
+export const icons = {
+    grandpa: <MdElderly size={35} />,
+    grandma: <MdElderlyWoman size={35} />,
+    man: <MdMan size={35} />,
+    woman: <MdWoman size={35} />,
+    "m-kid": <TbManFilled size={35} />,
+    "f-kid": <TbWomanFilled size={35} />,
+  };
